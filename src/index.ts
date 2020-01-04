fcedff6a7631fc15460b6732356a356b2d40f55a
Ilia Idakiev <ilia.idakiev@gmail.com>
2020-01-04 17:19:25 +0200

    commitjs-5

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
+import 'reflect-metadata';
+
+import './router/router-link.component';
+import './router/router.component';
+import './app.component';
